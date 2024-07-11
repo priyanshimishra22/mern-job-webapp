2ce66773a73e32783e3a99855132524432a53212
Priyanshi Mishra <priyanshi0322@gmail.com>
2024-07-11 17:59:19 +0530

    app.jsx initialised

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
-
+import React,{useEffect,useContext} from 'react'
+import './App.css';
+import {Context} from './main';
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
+import Home from './components/Home/Home';
+import Jobs from './components/Job/Jobs';
+import JobDetails from './components/Job/JobDetails';
+import MyJobs from './components/Job/MyJobs';
+import PostJobs from './components/Job/PostJob';
+import Application from './components/Application/Application';
+import MyApplications from './components/Application/MyApplications';
+import NotFound from './components/NotFound/NotFound';
+import axios from "axios";
+import { Toaster} from "react-hot-toast";
 const App = () => {
-  return (
+
+  const {isAuthorized,setIsAuthorized,setUser} = useContext(Context);
+  useEffect (()=>{
+      const fetchUser = async() =>{
+        try{
+          const response = axios.get("", {withCredentials:true})
+          setUser(response.data.user)
+          setIsAuthorized(true);
+
+        }catch(error){
+              setIsAuthorized(false);
+        }
+
+      }
+      fetchUser();
+    }, [isAuthorized])
+
+
+
+
+
+
+
+
+  return 
     <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/job/getall" element={<Jobs/>}/>
+        <Route path="/job/:id" element={<JobDetails/>}/>
+        <Route path="/job/post" element={<PostJobs/>}/>
+        <Route path="/job/me" element={<MyJobs/>}/>
+        <Route path="/application/:id" element={<Application/>}/>
+        <Route path="/application/me" element={<MyApplications/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+      <Footer/>
+      <Toaster/>
+    </Router>
     
     </>
-  )
+  
 }
 
 export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+export const Context = createContext({isAuthorized: false})
+const AppWrapper = ()=>{
 
+  const[isAuthorized, setIsAuthorized] = useState(false);
+  const [user, setUser] = useState({});
+
+  return (
+    <Context.Provider value={{isAuthorized, setIsAuthorized,user,setUser}}>
+      <App/>
+    </Context.Provider>
+  )
+
+
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppWrapper/>
+    
   </React.StrictMode>,
 )

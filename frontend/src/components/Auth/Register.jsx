a070c26eee238d9ceaa082b8df6db0fa0e9b68b0
Priyanshi Mishra <priyanshi0322@gmail.com>
2024-07-27 23:44:52 +0530

    register page

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
-
+import React,{useContext, useState} from 'react'
+import {Context} from '../../main'
+import axios from "axios";
+import toast from "react-hot-toast";
+import {Navigate} from "react-router-dom"
+import { FaPencilAlt, FaRegUser } from 'react-icons/fa';
+import {MdOutlineMailOutline} from 'react-icons/md'
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+
+  const [phone,setPhone] = useState("");
+
+  const [name,setName] = useState("");
+
+  const [role,setRole] = useState("");
+
+  const {isAuthorized,setIsAuthorized,user,setUser} = useContext(Context);
+
+  const handleRegister = async(e)=>{
+    e.preventDefault()
+    try{
+      const{data} = await axios.post(
+        "http://localhost:4000/api/v1/user/register",
+      {name,email,password,phone,role},
+      {
+        withCredentials:true,
+      headers:{
+        "Content-Type":"application/json",
+      }
+    }
+    )
+    toast.success(data.message);
+    setName("")
+    setEmail("");
+    setPhone("")
+    setRole("")
+    setPassword("")
+    setIsAuthorized(true);
+    }catch(error){
+      toast.error(error.response.data.message)
+    }
+  }
+  if(isAuthorized){
+    return <Navigate to={"/"}/>
+  }
+  return 
+    <>
+    <div className="authPage">
+      <div className="container">
+        <div className="header">
+          <img src="/JobZeelogo.png" alt="logo"/>
+          <h3>Create a new account</h3>
+        </div>
+        <form>
+          <div className="inputTag">
+            <label> Register As</label>
+            <div>
+              <select value={role} onChange={(e)=>setRole(e.target.value)}>
+                <option value="">Select Role</option>
+                <option value="Employer">Employer</option>
+                <option value="Job Seeker">Job Seeker</option>
+
+              </select>
+              <FaRegUser/>
+
+            </div>
+          </div>
+          <div className="inputTag">
+            <label> Name </label>
+            <div>
+              <input 
+              type="text" 
+              value={name} 
+              onChange={(e)=>setName(e.target.value)} 
+              placeholder="Priyanshi"/>
+              <FaPencilAlt/>
+              
+            </div>
+          </div>
+          <div className="inputTag">
+            <label> Email Address </label>
+            <div>
+              <input 
+              type="email" 
+              value={email} 
+              onChange={(e)=>setEmail(e.target.value)} 
+              placeholder="priyanshi@gmail.com"/>
+              <MdOutlineMailOutline/>
+              
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+
+    </>
+  
 }
 
 export default Register
